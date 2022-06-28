f6b9d8c67f49d7c005c501c2a1da94e7c46b7cd5
ge82ced <12344321.iI>
2022-06-28 17:40:18 +0200

    main

--- teduco-ui/src/App.js
+++ teduco-ui/src/App.js
@@ -5,6 +5,10 @@ import UserList from "./pages/home/userList/UserList";
 import Topbar from "./components/topbar/Topbar.jsx"
 import Sidebar from "./components/sidebar/Sidebar.jsx"
 import NewUser from "./pages/home/newUser/newUser.jsx";
+import NewProduct from "./pages/home/newProduct/NewProduct.jsx";
+import User from "./pages/home/user/User.jsx";
+import Signup from "./pages/home/newApplication/Signup.jsx";
+
 
 function App() {
   return (
@@ -16,6 +20,11 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/users" element={<UserList/>} />
         <Route path="/newuser" element={<NewUser/>} />
+        <Route path="/newproduct" element={<NewProduct/>} />
+        <Route path="/user" element={<User/>} />
+        <Route path="/newApplication" element={<Signup/>} />
+        <Route path="/test" element={<Topbar/>} />
+
       </Routes>
             </div>
 
--- teduco-ui/src/components/sidebar/Sidebar.jsx
+++ teduco-ui/src/components/sidebar/Sidebar.jsx
@@ -2,25 +2,64 @@ import './sidebar.css'
 import HomeIcon from '@mui/icons-material/Home';
 import ArticleIcon from '@mui/icons-material/Article';
 import SchoolIcon from '@mui/icons-material/School';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import {Route,Routes, useNavigate} from "react-router-dom"
+
 
 export default function Sidebar() {
+  const navigate = useNavigate();
+
+  const navigateToUsers = () => {
+    navigate('/users');
+  };
+
+  const navigateToApplications = () => {
+    navigate('/newApplication');
+  };
+
+  const navigateToUniversities = () => {
+    navigate('/universities');
+  };
+
+  const navigateToHome = () => {
+    navigate('/');
+  };
+
   return (
     <div className="sidebar">
         <div className="sidebarWrapper">
+        <div className="sidebar-logo">
+          <span className="logo">teduco</span>
+          </div>
+         
          <div className="sidebarMenu">
+
           <ul className="sidebarList">
 
-            <li className="sidebarListItem"> 
-            <span class="sidebarIcon"><HomeIcon /> </span>  
+            <li onClick={navigateToHome} className="sidebarListItem"> 
+            <span class="sidebarIcon"> <HomeIcon /> </span>
             Home
             </li>
 
-            <li className="sidebarListItem"> 
+            <li onClick={navigateToApplications} className="sidebarListItem"> 
             <span class="sidebarIcon"><ArticleIcon />  </span>  
             Applications
             </li>
 
+            <li  className="sidebarListItem"> 
+            <span class="sidebarIcon"><CalendarMonthIcon/>  </span>  
+            Calender
+            </li>
+
+
             <li className="sidebarListItem"> 
+            <span class="sidebarIcon"><SettingsIcon />   </span>  
+            Settings
+            </li>
+
+
+            <li onClick={navigateToUniversities} className="sidebarListItem"> 
             <span class="sidebarIcon"><SchoolIcon />  </span>  
             Universities
             </li>
