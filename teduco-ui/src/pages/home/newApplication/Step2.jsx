7d4887eb319b8d5c7a6999aec4afd07e0c49f7c1
Berkay Eren Ürün <beu1999@gmail.com>
2022-07-02 04:13:45 +0200

    half working dynamic status bar

--- teduco-ui/src/pages/home/newApplication/Step2.jsx
+++ teduco-ui/src/pages/home/newApplication/Step2.jsx
@@ -2,8 +2,10 @@ import "./step2.css";
 import { Component } from "react";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import "./progress-bar.css"
-const percentage = 76;
-const likelihood = "Good";
+let notes = 76;
+let exam = 50;
+let resultInt = 100;
+let resultText = "";
 
 export class Step2 extends Component {
     continue = e => {
@@ -30,26 +32,42 @@ export class Step2 extends Component {
         if(Exam_Result>400){second=4;}
         if(Exam_Result>450){second=5;}
 
-        if(first+second<2){return[20,"Very Unlikely"];}
+        if(first+second<=2){return[20,"Very Unlikely"];}
         if(first+second>2){return[40,"Unlikely"];}
         if(first+second>4){return[60,"Average"];}
         if(first+second>6){return[80,"Likely"];}
         if(first+second>8){return[90,"Very Likely"];}
         }
 
+    handleChange1 = (event) => {
+        notes = event.target.value;
+        resultInt = this.birsen(notes, exam)[0]
+        resultText = this.birsen(notes, exam)[1]
+        console.log("lool")
+
+    }
+
+    handleChange2 = (event) => {
+        exam = event.target.value;
+        resultInt = this.birsen(notes, exam)[0]
+        resultText = this.birsen(notes, exam)[1]
+
+    }
+
     render() {
         const { values, handleChange } = this.props;
+        
         return(
             <div>
             
         <form>
         <div className="choice" >
             <label>Note Average </label>
-            <input type="text" defaultValue={values.Note_Average} onChange={handleChange('Note_Average')} placeholder="82"></input>
+            <input type="text" defaultValue={values.notes} onBlur={this.handleChange1} placeholder="82"></input>
         </div>
         <div className="choice">
             <label>AYT/TYT Result</label>
-            <input type="text" defaultValue={values.Exam_Result} onChange={handleChange('Exam_Result')} placeholder="382"></input>
+            <input type="text" defaultValue={values.exam} onBlur={this.handleChange2} placeholder="382"></input>
         </div>
 
         <div className="choice">
@@ -72,7 +90,7 @@ export class Step2 extends Component {
         <p  onClick={this.continue} className="next">Next</p>
         </form>
 
-        <CircularProgressbar value={this.birsen()} text={`${likelihood}`} />
+        <CircularProgressbar value={resultInt} text={`${resultText}`} />
         <label class="progress-bar-label">Probality of Admission</label>
 
        
